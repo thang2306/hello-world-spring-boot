628bd2c601a400741872724afac7cebf36f907f5
Thang Nguyen <you@example.com>
2021-05-03 05:40:02 +0000

    3

--- src/main/java/io/datawire/labs/hellospring/HelloController.java
+++ src/main/java/io/datawire/labs/hellospring/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
                                       TimeUnit.MILLISECONDS.toMinutes(millis),
                                       TimeUnit.MILLISECONDS.toSeconds(millis) -
                                       TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-        return String.format("Hello 2 commit, Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
+        return String.format("Hello 3 commit, Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
     }
 
 }
